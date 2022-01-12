eaf77f486eb42534164952d5b00caaa894055d0e
Isaac Green <zuderg@gmail.com>
2022-01-12 12:16:14 -0600

    added task 2

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,30 @@
+
+//http://0.0.0.0:5001/api/v1/status/
+$(document).ready(function () {
+    const newDic = {};
+    $('input').click(function () {
+      $(':input').each(function () {
+        if (this.checked === true) {
+          newDic[$(this).data('name')] = $(this).data('id');
+        } else {
+          delete newDic[$(this).data('name')];
+        }
+      });
+      const list = [];
+      for (const key in newDic) {
+        list.push(key);
+      }
+      $('.amenities h4').html(list.join(', '));
+    });
+
+    $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+    if (data.status === "OK") {
+        $('#api_status').addClass("available");
+    }
+    else {
+        $('#api_status').removeClass("available");
+    }
+
+    });
+
+});
