aced35e6d92f9b3253de42ebbff013d2feb79aa9
lyndonpett <lyndonpett98@gmail.com>
2022-01-13 11:57:24 -0600

    Adding the finishing task 3

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,30 +1,28 @@
-
 //http://0.0.0.0:5001/api/v1/status/
 $(document).ready(function () {
-    const newDic = {};
-    $('input').click(function () {
-      $(':input').each(function () {
-        if (this.checked === true) {
-          newDic[$(this).data('name')] = $(this).data('id');
-        } else {
-          delete newDic[$(this).data('name')];
-        }
-      });
-      const list = [];
-      for (const key in newDic) {
-        list.push(key);
+  const newDic = {};
+  $('input').click(function () {
+    $(':input').each(function () {
+      if (this.checked === true) {
+        newDic[$(this).data('name')] = $(this).data('id');
+      } else {
+        delete newDic[$(this).data('name')];
       }
-      $('.amenities h4').html(list.join(', '));
     });
-
-    $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
-    if (data.status === "OK") {
-        $('#api_status').addClass("available");
+    const list = [];
+    for (const key in newDic) {
+      list.push(key);
     }
-    else {
-        $('#api_status').removeClass("available");
+    $('.amenities h4').html(list.join(', '));
+  });
+
+  $.get('http://127.0.0.1:5001/api/v1/status/', function (data) {
+    // maybe switch to 0.0.0.0
+    console.trace(data);
+    if (data.status === 'OK') {
+      $('#api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
     }
-
-    });
-
+  });
 });
