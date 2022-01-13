152259f7f3a29e1a7ae1d357dcf4918ebab9dff6
Isaac Green <zuderg@gmail.com>
2022-01-13 13:47:55 -0600

    added task 4

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,37 +1,63 @@
-//http://0.0.0.0:5001/api/v1/status/
+// http://0.0.0.0:5001/api/v1/status/
 $(document).ready(function () {
-    const newDic = {};
-    $('input').click(function () {
-      $(':input').each(function () {
-        if (this.checked === true) {
-          newDic[$(this).data('name')] = $(this).data('id');
-        } else {
-          delete newDic[$(this).data('name')];
-        }
-      });
-      const list = [];
-      for (const key in newDic) {
-        list.push(key);
-      }
-      $('.amenities h4').html(list.join(', '));
-    });
-
-    $.get('http://127.0.0.1:5001/api/v1/status/', function (data) {
-      // maybe switch to 0.0.0.0
-      if (data.status === 'OK') {
-        $('#api_status').addClass('available');
+  const newDic = {};
+  $('input').click(function () {
+    $(':input').each(function () {
+      if (this.checked === true) {
+        newDic[$(this).data('name')] = $(this).data('id');
       } else {
-        $('#api_status').removeClass('available');
+        delete newDic[$(this).data('name')];
       }
     });
-    $.post({url: 'http://127.0.0.1:5001/api/v1/places_search/', data: {}, /*type: 'POST',*/ dataType: 'application/json', success: function(data) {
-        for (let i = 0; i < data.length; i++) {
-        console.log(data[i])
-        }
-    }})
+    const list = [];
+    for (const key in newDic) {
+      list.push(key);
+    }
+    $('.amenities h4').html(list.join(', '));
   });
 
-  /*
+  $.get('http://127.0.0.1:5001/api/v1/status/', function (data) {
+    // maybe switch to 0.0.0.0
+    if (data.status === 'OK') {
+      $('#api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
+    }
+  });
+  $.ajax({
+    method: 'POST',
+    url: 'http://127.0.0.1:5001/api/v1/places_search/',
+    contentType: 'application/json',
+    data: '{}',
+    success: function (data) {
+      for (let i = 0; i < data.length; i++) {
+        const place = data[i];
+        console.log(place);
+        $.get('http://127.0.0.1:5001/api/v1/users/' + place.user_id, function (data) {
+            console.log(data)
+            let person_first_name = data.first_name;
+            let person_last_name = data.last_name;
+        $('.places').append(
+            '<article>' +
+            '<div class="title_box"><h2>' + place.name + '</h2>' +
+            '<div class="price_by_night">$' + place.price_by_night + '</div></div>' +
+            '<div class="information"><div class="max_guest">' + place.max_guest + (place.max_guest != 1 ? ' Guests' : ' Guest') + "</div>" +
+            '<div class="number_rooms">' + place.number_rooms + (place.number_rooms != 1 ? ' Bedrooms' : ' Bedroom') + '</div>' +
+            '<div class="number_bathrooms">' + place.number_bathrooms + (place.number_rooms != 1 ? ' Bathrooms' : ' Bathroom') + '</div>' + '</div>' +
+            '<div class="user"><b>Owner: </b>' + person_first_name + ' ' + person_last_name + '</div>' +
+            '<div class="description">' + place.description + '</div>' +
+            '</article>'
+
+
+            )
+        });
+
+      }
+    }
+  });
+});
+
+/*
   <article>
 	  <div class="title_box">
 	    <h2>PLACENAME</h2>
@@ -39,8 +65,8 @@ $(document).ready(function () {
 	  </div>
 	  <div class="information">
 	    <div class="max_guest">MAXGUEST GuestADDS)</div>
-            <div class="number_rooms">NUMROOMS BedroomADDS</div>
-            <div class="number_bathrooms">{{ place.number_bathrooms }} Bathroom{% if place.number_bathrooms != 1 %}s{% endif %}</div>
+        <div class="number_rooms">NUMROOMS BedroomADDS</div>
+        <div class="number_bathrooms">{{ place.number_bathrooms }} Bathroom{% if place.number_bathrooms != 1 %}s{% endif %}</div>
 	  </div>
 	  <div class="user">
             <b>Owner:</b> {{ place.user.first_name }} {{ place.user.last_name }}
@@ -49,3 +75,5 @@ $(document).ready(function () {
           </div>
 	</article>
     */
+
+    // IP:PORT/users/<user_id>
